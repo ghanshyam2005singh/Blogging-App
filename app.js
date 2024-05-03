a5a7ebedf668c69153272a7c3d809d8e66ea11b6
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2024-05-03 17:35:44 +0530

    completed

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require ("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const Blog = require("./models/blog");
+
 const userRouter = require("./routes/user");
 const blogRoute = require("./routes/blog");
 
-const app = express();
-const PORT = 8000;
+const { checkForAuthenticationCookie } = require("./middleware/authentication");
 
-mongoose.connect("mongodb://localhost:27017/bloggers").then( e => console.log("MongoDB is Connected"));
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.Mongo_URL).then((e) => console.log("MongoDB is Connected"));
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
-    res.render("home");
-    user: req.user;
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
 });
 
 app.use("/user", userRouter);
