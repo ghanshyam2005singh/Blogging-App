1870b64469c54b5569e66e0dc2a137d8dbfb8f63
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2024-05-03 17:09:39 +0530

    completed

--- services/authentication.js
+++ services/authentication.js
@@ -6,19 +6,19 @@ function createTokenForUser(user) {
     const payload = {
         _id: user._id,
         email: user.email,
-        profileImageURL: user.profileImageURL: user.profileImageURL,
+        profileImageURL: user.profileImageURL,
         role: user.role,
     };
     const token = JWT.sign(payload, secret);
     return token;
 }
 
-function verifyToken(token) {
-    return JWT.verify(token, secret);
+function validateToken(token) {
+    const payload=  JWT.verify(token, secret);
     return payload;
 }
 
     module.exports = {
         createTokenForUser,
         verifyToken,
-    }
+    };
